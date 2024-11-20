a8a816c09d5f854b2dfc1647f4d5722d9950bce6
sachin <mauryasachin243601@gmail.com>
2024-11-21 01:05:40 +0530

    ComplainModel.js

---
+++ Backend/models/ComplaintModel.js
@@ -0,0 +1,86 @@
+import mongoose from 'mongoose';
+
+const complaintSchema = new mongoose.Schema({
+    category: {
+        categoryType: {
+            type: String,
+            enum: ['personal', 'common'],
+            required: true,
+        },
+        subCategoryType: {
+            type: String,
+            required: function () {
+                return this.category.categoryType === 'personal' || this.category.categoryType === 'common';
+            },
+            validate: {
+                validator: function () {
+                    // Define your validation logic here
+                    if (this.category.categoryType === 'personal') {
+                        return ['electricity', 'civil', 'cleaning', 'other'].includes(this.category.subCategoryType);
+                    } else if (this.category.categoryType === 'common') {
+                        return ['water', 'bathroom', 'light', 'other'].includes(this.category.subCategoryType);
+                    } else {
+                        return false;
+                    }
+                },
+            }
+        }
+    },
+    currentStatus: {
+        type: String,
+        enum: ['pending', 'ongoing', 'solved', 'rejected'],
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "User",
+    },
+    receivedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Caretaker",
+    },
+    warden: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Warden",
+    },
+    hostel: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "Hostel",
+    },
+    upvotes: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ],
+    comments: [
+        {
+            comment: String,
+            user: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        }
+    ],
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    image: {
+        type: String,
+    }
+});
+
+export default mongoose.model("Complaint", complaintSchema);
